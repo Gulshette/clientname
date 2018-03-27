caa44592c09e337e4f7db01b950064096ec24afb
Smitha Kiran <you@example.com>
2018-03-27 16:26:13 +0530

    Menu component created

---
+++ clientname-core/src/main/groovy/com/clientname/components/content/menublock/MenuBlock.groovy
@@ -0,0 +1,44 @@
+package com.clientname.components.content.menublock
+
+import com.citytechinc.cq.component.annotations.DialogField
+import com.citytechinc.cq.component.annotations.Listener
+import com.citytechinc.cq.component.annotations.Tab
+import com.clientname.annotations.NilayaComponent
+import com.icfolson.aem.library.api.components.annotations.AutoInstantiate
+import com.icfolson.aem.library.core.components.AbstractComponent
+import com.icfolson.aem.multicompositeaddon.widget.MultiCompositeField
+
+
+/**
+ * Created by icf2025295 on 21/03/18.
+ */
+@NilayaComponent(value = "Menu Block",
+        name = "menublock",
+        group = "ap-nilaya",
+        tabs = [
+                @Tab(title = "Menu Block"),
+                @Tab(title = "Dropdown Block")
+        ],
+        listeners = [
+                @Listener(name = "afteredit", value = "REFRESH_PAGE"),
+                @Listener(name = "afterinsert", value = "REFRESH_PAGE"),
+                @Listener(name = "afterdelete", value = "REFRESH_PAGE")
+        ])
+@AutoInstantiate
+class MenuBlock extends AbstractComponent {
+
+
+    @DialogField(fieldLabel = "Menu Block Fields",ranking = 1D, tab = 1)
+
+    @MultiCompositeField
+    List<MenuBlockFields> getMenuBlockFields() {
+        getNodesInherited("menuBlockFields").collect { child -> child.resource.adaptTo(MenuBlockFields) }
+    }
+
+    @DialogField(fieldLabel = "Dropdown Fields",ranking = 2D, tab = 2)
+    @MultiCompositeField
+    List<DropdownFields> getDropdownFields() {
+        getNodesInherited("dropdownFields").collect { child -> child.resource.adaptTo(DropdownFields) }
+    }
+
+}
